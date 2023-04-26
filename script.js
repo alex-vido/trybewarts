aecacd117b5dd5f587f2e4bf6b7f474a8988bcb5
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-04-26 15:59:38 -0300

    ate o 4 funcional

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnLogin = document.getElementById('btn-login');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+btnLogin.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(email.value === 'tryber@teste.com' && password.value === '123456'){
+        alert ('Olá, Tryber!');
+    } else{
+        alert('Email ou senha inválidos.')
+    }
+
+})
